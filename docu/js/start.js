eaea5e4796f69fd340b513dd37660bdcb98077b1
DESKTOP-J1FOOED\nan-minipc <daniel@sestera.com>
2017-12-17 14:30:43 +0100

    no message

--- docu/js/start.js
+++ docu/js/start.js
@@ -1,1 +1,12 @@
  
+$(document).ready(function(){
+    
+    $("* [data-toggle='collapse']").click(function(e){
+        
+        var el = e.target;
+        var collapse = el.getAttribute("data-target");
+        $(collapse).collapse();
+        
+    });
+    
+});
